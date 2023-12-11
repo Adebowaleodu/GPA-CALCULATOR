0aa9ab5e0012050ea16f6f37215fc330979e91ff
Adebowale Odu <88664550+Adebowaleodu@users.noreply.github.com>
2023-12-11 08:29:38 +0100

    Add files via upload

---
+++ GPACALCULATOR/src/gpacalculator/LetStart.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gpacalculator;
+
+/**
+ *
+ * @author DELL
+ */
+class LetStart {
+
+    String getGrade(double score1) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    int getPoint(String grade1) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
